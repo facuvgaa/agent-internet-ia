ce979bc5e9ff1aad2458b62399e8b2f49037e313
Tu Nombre <you@example.com>
2026-03-10 02:20:11 -0300

    first commit

---
+++ cliente_back/src/main/java/com/cliente/cliente_back/ClienteBackApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.cliente_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteBackApplication.class, args);
+	}
+
+}
